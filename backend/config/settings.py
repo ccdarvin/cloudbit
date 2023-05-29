e40be00e990712876fc2e6bfee07e8cf0b6bf7a9
Darvin Cotrina <cc.darvin@gmail.com>
2023-05-29 16:45:59 -0500

    change backend folder

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str | None = None
+    db_url: str 
 
     class Config:
         env_file = ".env"
