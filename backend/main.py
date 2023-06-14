d1ba13132b5444b91b975b65376a4deb58d2e1f5
Darvin <cc.darvin@gmail.com>
2023-06-13 23:21:15 -0500

    add Cors

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.app import app as auth_app
 
 from fastapi_pagination import add_pagination
 
 
-
 app = auth_app
 add_pagination(app)
+
+# configure CORS
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
